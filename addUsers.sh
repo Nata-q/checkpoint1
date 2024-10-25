b8edea09e97b0375eaa89fa221be50cbe828d449
Nata-q <ant.turchi@gmail.com>
2024-10-25 12:56:59 +0200

    Update addUsers.sh

--- addUsers.sh
+++ addUsers.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+#vérifier si les éléments rentrés sont valide
+[ $# -eq 0 ] && { echo > Il manque les noms d'utilisateurs en argument\nFin du script. ; exit1 ; }
+user=$@
+
+#Création boucle pour lire les arguments 1 a 1
+while true; do
+
+#Vérification si l'utilisateur existe déjà
+if
+  cat /etc/passwd | grep $user
+then
+#le compte existe
+#affichage message et sortie du script
+  echo -e "l'utilisateur $user existe."
+exit 1
+
+else
+#le compte n'existe pas
+#affichage message et création de compte
+  sudo useradd $user > /dev/null
+  echo "utilisateur $user créé."
+fi
+done
+exit 0
